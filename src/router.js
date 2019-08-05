f19789adfd8096a5a655d012a0976c553aae30ed
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-08-05 15:31:12 +0800

    first

--- src/router.js
+++ src/router.js
@@ -5,11 +5,15 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/buy',
+      name: 'Buy Tickets',
+      component: () => import('./views/BuyTix.vue')
     },
     {
       path: '/about',
@@ -17,7 +21,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+
   ]
 })
